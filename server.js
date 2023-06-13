729524cfb6c1514fdf42a5fdc40191a415ad689f
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-06-13 10:03:27 -0500

    started promptuser function

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const inquirer = require('inquirer');
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
 
-const PORT = 3001;
-const HOST = 'localhost';
-app.listen(PORT, HOST, () => {
+const PORT = process.env.PORT || 3001;
+const app = express(); 
 
-const query = require('./db/query.sql');
-const db = require('./db');
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const connection = mysql.createConnection(
+    {
+    host: 'localhost',
+    user: 'root',
+    database: 'employee_manager_db',
+    password: 'password',
+    },
+    console.log(`Connected to the employee_manager_db database.`)
+);
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'toDo',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role', 
+                'View All Departments', 
+                'Add Department', 
+                'Quit'
+            ],
+        },
