b25d3e3ee8cd3f9b9cb334989d1846720d44e29e
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-06-13 11:48:17 -0500

    finished inquirer sequence, now to translate that into sql queries

--- server.js
+++ server.js
@@ -36,5 +36,96 @@ const promptUser = () => {
                 'View All Departments', 
                 'Add Department', 
                 'Quit'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the their first name?',
+            when: (answers) => answers.toDo === 'Add Employee'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the their last name?',
+            when: (answers) => typeof answers.firstName === 'string'
+        },
+        {
+            type: 'list',
+            name: 'newEmployeeRole',
+            message: "What is the employee's role?",
+            choices: [
+                'Sales Lead',
+                'Salesperson',
+                'Lead Engineer',
+                'Software Engineer',
+                'Account Manager',
+                'Accountant',
+                'Legal Team Lead',
+                'Lawyer',
+                'Customer Service Agent',
+            ],
+            when: (answers) => typeof answers.lastName === 'string'
+        },
+        {
+            type: 'list',
+            name: 'newEmployeeManager',
+            message: "Who is the employee's manager?",
+            choices: [
+                'John Doe',
+                'Mike Chan',
+                'Ashley Rodriguez',
+                'Kevin Tupik',
+                'Malia Brown',
+                'Sarah Lourd',
+            ],
+
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter the Employee Id of the employee you would like to update.',
+            when: (answers) => answers.toDo === 'Update Employee Role'
+        },
+        {
+            type: 'input',
+            name: 'employeeRoleUpdate',
+            message: 'What is the new role of the employee?',
+            when: (answers) => typeof answers.employeeId === 'number'
+        },
+        {
+            type: 'input',
+            name: 'addRole',
+            message: 'What is the title of the role you would like to add?',
+            when: (answers) => answers.toDo === 'Add Role'
+        },
+        {
+            type: 'input',
+            name: 'addSalary',
+            message: 'What is the salary of this role?',
+            when: (answers) => typeof answers.addRole === 'string'
+        },
+        {
+            type: 'list',
+            name: 'departmentSelect',
+            message: 'What department does this role belong to?',
+            choices: [
+                'Engineering',
+                'Finance',
+                'Legal',
+                'Sales',
+                'Service',
             ],
         },
+        {
+            type: 'input',
+            name: 'addDepartment',
+            message: 'What is the title of the department you would like to add?',
+            when: (answers) => answers.toDo === 'Add Department'
+        },
+        {
+            type: 'input',
+
+        }
+    ])
+}
